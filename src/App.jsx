e9f4733ff0d8b00e41b7780e37d0d379cee6ae92
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-11-14 23:34:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Die from './components/Die'
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(generateDice())
+
+    function generateDice() {
+        let dice = []
+        for (let i = 0; i < 10; i++) {
+            const die = {
+                value: randomDieNum(),
+                isHeld: false
+            }
+            dice = [...dice, die]
+            console.log(dice)
+        }
+        return dice
+    }
+
+    function randomDieNum() {
+        return Math.ceil(Math.random() * 6)
+    }
+
+
+    const diceElement = dice.map((die, index) => {
+        return (
+            <Die
+                value={die.value}
+                key={index}
+            />
+        )
+    }
+    )
+
+    return (
+        <main>
+            <h1>Tenzies</h1>
+            <p>Roll untill all dice are the same value.
+                Click each die to freeze it at its current value between rolls.</p>
+
+            <div className="dice-container">
+                {diceElement}
+            </div>
+        </main>
+    )
+}
