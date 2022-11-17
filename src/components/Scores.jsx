464940984fe369e84dbe9621246a11b43842dd46
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-11-16 21:43:50 -0300

    improve time tracker and Scores style

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import './Scores.css'
 
-export default function Scores (props) {
+export default function Scores(props) {
+
+    function millisToMinutesAndSeconds(millis) {
+        let minutes = Math.floor(millis / 60000)
+        let seconds = ((millis % 60000) / 1000).toFixed(0)
+        return (
+            seconds == 60 ?
+                (minutes + 1) + ":00" :
+                `${minutes < 10 ? '0' : ''}${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`
+        )
+    }
+    const gameElapsedTime = millisToMinutesAndSeconds(props.currentGameTime)
+
     return (
         <div className="score-container">
-                <div className="roll-tracker">
-                    {`number of rolls: ${props.rollCounter}`}
-                </div>
-
-                <div className="time-container">
-                    elapsed time: {props.currentGameTime ? 
-                        `${parseInt(props.currentGameTime / 600)} : ${parseInt(props.currentGameTime % 60)}` :
-                        0
-                    }
-                </div>
+            <div className="roll-track-container">
+                rolls: {props.rollCounter}
             </div>
+
+            <div className="time-track-container">
+                time: {props.currentGameTime ?
+                    gameElapsedTime :
+                    '00:00'
+                }
+
+            </div>
+        </div>
     )
 }
